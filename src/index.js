2b068670e9e34336fe6e29042ca605a482993f28
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-09-22 00:10:35 +0500

    set date and time with react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+const name = "Umair Nisar";
+const currdate = new Date().toLocaleDateString();
+const currtime = new Date().toLocaleTimeString();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+ <h1>My name is {name} </h1>
+ <p>The  Date is =  {currdate} </p>
+ <p>The Time Is  = {currtime}</p>
+
+  </>,
+
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
